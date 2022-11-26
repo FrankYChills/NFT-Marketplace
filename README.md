327e5adf82b6e06a7c372a024abb8f1c2bcc98de
franky <itskchills11@gmail.com>
2022-11-26 13:15:12 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Create a decentralized NFT Marketplace
+
+That will do the followings -
+
+```
+1. Users can list NFTs on the Marketplace
+2. Users can buy NFTs
+3. Users can Cancel any listings
+4. Update Pricing of an NFT
+5. Withdraw payments for sold NFTs
+```
---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,133 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// import interface
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+error NftMarketplace__PriceBelowZero();
+error NftMarketplace__MarketplaceNotApproved();
+error NftMarketplace__NftAlreadyListed();
+error NftMarketplace__NotOwner();
+error NftMarketplace__NftNotListed();
+error NftMarketplace__InsufficientTransfer(
+    address nftAddress,
+    uint256 tokenId,
+    uint256 nftPrice
+);
+
+contract NftMarketplace {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    event ItemBought(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 nftPrice
+    );
+
+    //Listed NFT contract -> (NFT TokenId(which NFT) -> Listing)
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+    //Seller Address => Amount Earned   (Amount to be given by marketplace to the seller)
+    mapping(address => uint256) private s_lockedValue;
+
+    constructor() {}
+
+    // modifier to make sure that same nft shouldnt be listed twice
+    modifier isListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketplace__NftAlreadyListed();
+        }
+        _;
+    }
+
+    // only the owner(minter) of the nft should be able to list the nft on the marketplace
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address sender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        // get owner via tokenId
+        address owner = nft.ownerOf(tokenId);
+        if (sender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    //before purchasing any nft make sure its listed
+    modifier NotListed(address nftAddress, uint256 tokenId) {
+        Listing memory nft = s_listings[nftAddress][tokenId];
+        if (nft.price <= 0) {
+            revert NftMarketplace__NftNotListed();
+        }
+        _;
+    }
+
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        isOwner(nftAddress, tokenId, msg.sender)
+        isListed(nftAddress, tokenId, msg.sender)
+    {
+        if (price <= 0) {
+            revert NftMarketplace__PriceBelowZero();
+        }
+        IERC721 nft = IERC721(nftAddress);
+        // check if the NFT to be listed approves marketplace
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketplace__MarketplaceNotApproved();
+        }
+        // Update the Data Structures
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    ) external payable nonReentrant NotListed(nftAddress, tokenId) {
+        //get the item user wants to purchase
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        // revert if less amount is pais
+        if (msg.value < listedItem.price) {
+            revert NftMarketplace__InsufficientTransfer(
+                nftAddress,
+                tokenId,
+                listedItem.price
+            );
+        }
+        // update lockedValue of seller
+        s_lockedValue[listedItem.seller] =
+            s_lockedValue[listedItem.seller] +
+            msg.value;
+        // as nft has been been sold remove that from data structure
+        delete (s_listings[nftAddress][tokenId]);
+        // now transfer the NFT to the purchaser account
+        IERC721(nftAddress).safeTransferFrom(
+            listedItem.seller, // from
+            msg.sender, // to
+            tokenId //specifies which nft to tranfer
+        );
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
+}
